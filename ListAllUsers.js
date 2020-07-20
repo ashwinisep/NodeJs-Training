8694ce468357ce1f959109f66c7f32304c192864
ashwinisep <60849266+ashwinisep@users.noreply.github.com>
2020-07-20 18:17:39 +0530

    Committed Get,Post method samples

---
+++ ListAllUsers.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var user = {
+    "user4" : {
+       "name" : "mohit",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+    }
+ }
+ 
+//Get
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "UsersList.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+//POst
+app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "UsersList.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
+    });
+ })
+
+ //Serach a user based on a ID
+ app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "UsersList.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+
+var server = app.listen(4082, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
